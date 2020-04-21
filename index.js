49be2f383480064b9aafb9d5a20047c49deb5d13
Ekaansh Arora <blue.name68@gmail.com>
2020-04-22 02:04:15 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import {AppRegistry} from 'react-native';
+import Video from './components/Video';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Video);
